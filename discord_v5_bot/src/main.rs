4870eb5fb4fbed9e87bab73e48dc06102d7915f8
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-07-19 10:03:30 -0500

    Add black/red dice rolling

--- discord_v5_bot/src/main.rs
+++ discord_v5_bot/src/main.rs
@@ -1,10 +1,90 @@
+use rand::prelude::*;
 use serenity::async_trait;
 use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
+use serenity::model::interactions::application_command::{
+    ApplicationCommandInteractionDataOptionValue, ApplicationCommandOptionType,
+};
 use serenity::model::interactions::{Interaction, InteractionResponseType};
 use serenity::prelude::*;
 use std::env;
-use rand::prelude::*;
+
+fn roll_dice(black_num: u8, red_num: u8) -> std::string::String {
+    let mut rng = thread_rng();
+    let mut black_digits: Vec<u8> = Vec::new();
+    for _ in 0..black_num {
+        black_digits.push(rng.gen_range(1..=10));
+    }
+    black_digits.sort();
+    black_digits.reverse();
+
+    let mut successes: u8 = 0;
+    let mut crits: u8 = 0;
+    let mut output = "".to_owned();
+
+    for b in black_digits {
+        if b == 10 {
+            successes += 1;
+            crits += 1;
+            output.push_str("<:Stars:727127987043958817> ");
+        } else if b > 5 {
+            successes += 1;
+            output.push_str("<:AnkhBlack:727127902490984450> ");
+        } else {
+            output.push_str("<:BlankBlack:727127939413704704> ");
+        }
+    }
+
+    let mut red_digits: Vec<u8> = Vec::new();
+    for _ in 0..red_num {
+        red_digits.push(rng.gen_range(1..=10));
+    }
+    red_digits.sort();
+    red_digits.reverse();
+
+    let mut has_skull = false;
+    let mut has_fangs = false;
+
+    for r in red_digits {
+        if r == 10 {
+            successes += 1;
+            crits += 1;
+            has_fangs = true;
+            output.push_str("<:Fangs:727127963639742506> ");
+        } else if r > 5 {
+            successes += 1;
+            output.push_str("<:AnkhRed:727127924721057813> ");
+        } else if r == 1 {
+            has_skull = true;
+            output.push_str("<:Skull:727127976352940132> ");
+        } else {
+            output.push_str("<:BlankRed:727127952243949610> ");
+        }
+    }
+
+    let mut is_messy = false;
+
+    while crits >= 2 {
+        successes += 2;
+        crits -= 2;
+        if has_fangs {
+            is_messy = true;
+        }
+    }
+
+    let successes_count_str = format!(", {} successes", successes);
+    output.push_str(&successes_count_str);
+
+    if is_messy {
+        output.push_str(", **messy**");
+    }
+
+    if has_skull {
+        output.push_str(", **bestial**");
+    }
+
+    output
+}
 
 struct Handler;
 
@@ -20,7 +100,40 @@ impl EventHandler for Handler {
                 "d10" => {
                     let face: u8 = thread_rng().gen_range(1..=10);
                     face.to_string()
-                },
+                }
+                "roll" => {
+                    let black_dice = match command
+                        .data
+                        .options
+                        .get(0)
+                        .expect("Expected number of black dice")
+                        .resolved
+                        .as_ref()
+                        .expect("Expected number of black dice")
+                    {
+                        ApplicationCommandInteractionDataOptionValue::Integer(black_dice) => {
+                            *black_dice as u8
+                        }
+                        _ => 0,
+                    };
+
+                    let red_dice = match command
+                        .data
+                        .options
+                        .get(1)
+                        .expect("Expected number of red dice")
+                        .resolved
+                        .as_ref()
+                        .expect("Expected number of red dice")
+                    {
+                        ApplicationCommandInteractionDataOptionValue::Integer(red_dice) => {
+                            *red_dice as u8
+                        }
+                        _ => 0,
+                    };
+
+                    roll_dice(black_dice, red_dice)
+                }
                 _ => "Not implemented".to_string(),
             };
 
@@ -56,7 +169,26 @@ impl EventHandler for Handler {
                     command.name("smile").description("Bot smiles back")
                 })
                 .create_application_command(|command| {
-                    command.name("d10").description("Bot rolls a ten-sided die")
+                    command.name("d10").description("Roll a ten-sided die")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("roll")
+                        .description("Roll black and red dice together")
+                        .create_option(|option| {
+                            option
+                                .name("black dice")
+                                .description("Number of black dice to roll")
+                                .kind(ApplicationCommandOptionType::Integer)
+                                .required(true)
+                        })
+                        .create_option(|option| {
+                            option
+                                .name("red dice")
+                                .description("Number of red dice to roll")
+                                .kind(ApplicationCommandOptionType::Integer)
+                                .required(true)
+                        })
                 })
         })
         .await;
